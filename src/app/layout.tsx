b4360c3dbbc2dc518db98079a247bb5421b13578
sans221131 <somaanransari@gmail.com>
2026-01-31 16:00:05 +0530

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import EnquireScroll from "@/components/EnquireScroll";
+import Script from "next/script";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -27,6 +28,18 @@ export default function RootLayout({
 		<html lang="en">
 			<head>
 				<link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
+				<Script
+					src="https://www.googletagmanager.com/gtag/js?id=AW-17892898232"
+					strategy="afterInteractive"
+					async
+				/>
+				<Script
+					id="gtag-init"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date());\n\ngtag('config', 'AW-17892898232');`,
+					}}
+				/>
 			</head>
 			<body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<EnquireScroll />
